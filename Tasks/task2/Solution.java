5c31ceaa5ccd7d352a1394833953117538d0f68e
mrwebbeast <mrwebbeast@gmail.com>
2021-12-19 19:34:46 +0530

    task 2

---
+++ Tasks/task2/Solution.java
@@ -0,0 +1,20 @@
+package task2;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        double b = scan.nextDouble();
+        scan.nextLine();
+        String c = scan.nextLine();
+        scan.close();
+
+        System.out.println(c + "");
+        System.out.println(b);
+        System.out.println(a);
+
+    }
+}
