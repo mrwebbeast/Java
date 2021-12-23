eb3958053c2ddd5246cc0334ab2545d62d8a7375
mrwebbeast <mrwebbeast@gmail.com>
2021-12-23 16:36:29 +0530

    Task 1

---
+++ Tasks/MrWebBeast.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MrWebBeast {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+
+        System.out.println(i + 4);
+        System.out.println(d + d);
+        System.out.printf("MrWebBeast %s", s);
+        scan.close();
+    }
+
+}
---
+++ Tasks/task16/Solution.java
@@ -0,0 +1,34 @@
+package task16;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        String[] s = new String[n + 2];
+        for (int i = 0; i < n; i++) {
+
+            s[i] = scan.next();
+
+        }
+        scan.close();
+        Comparator<String> myComparator = (s1, s2) -> {
+            BigDecimal a = new BigDecimal(s1);
+            BigDecimal b = new BigDecimal(s2);
+            return b.compareTo(a);
+        };
+        Arrays.sort(s, 0, n, myComparator);
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%s%n", s[i]);
+
+        }
+
+    }
+
+}
