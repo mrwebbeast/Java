538e7b37a7ff639a5b77fe1446f2036a5ad73357
Sahil Ranout <mrwebbeast@gmail.com>
2022-02-21 18:05:39 +0530

    Something New

---
+++ Tasks/task17/Solution.java
@@ -0,0 +1,13 @@
+package task17;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] list = scan.nextLine().split("");
+        System.out.println(list);
+        scan.close();
+    }
+}
