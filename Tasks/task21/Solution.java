a7d9f08bcab10416e5adc3f7982075fa121a3b69
mrwebbeast <mrwebbeast@gmail.com>
2022-07-17 16:01:26 +0530

    is Penadrom Problem Solved

---
+++ Tasks/task21/Solution.java
@@ -0,0 +1,27 @@
+package task21;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        String isPelendrom = isPelendrom(str) == true ? "Yes" : "No";
+        System.out.println(isPelendrom);
+        scan.close();
+    }
+
+    public static boolean isPelendrom(String str) {
+        String reverse = "";
+        boolean isPelendrom = false;
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse = reverse + str.charAt(i);
+        }
+
+        if (str.equals(reverse)) {
+            isPelendrom = true;
+        }
+        return isPelendrom;
+    };
+}
